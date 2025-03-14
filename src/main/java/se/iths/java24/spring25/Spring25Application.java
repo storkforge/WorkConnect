6b88a3f86d26dfa74aa7358ccb641d5afecedee9
Martin Blomberg <martin.blomberg@outlook.com>
2025-03-14 14:08:12 +0100

    Initial Commit

---
+++ src/main/java/se/iths/java24/spring25/Spring25Application.java
@@ -0,0 +1,13 @@
+package se.iths.java24.spring25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring25Application.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/java24/spring25/Spring25ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.java24.spring25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class Spring25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
