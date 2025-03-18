9c2d9c5d4dad0efac26fb431b3ed8fe2b36cf96a
Martin Blomberg <martin.blomberg@outlook.com>
2025-03-18 11:14:22 +0100

    Add Playground entity and migration

---
+++ src/main/java/se/iths/java24/spring25/domain/Playground.java
@@ -0,0 +1,14 @@
+package se.iths.java24.spring25.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Playground {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+}
