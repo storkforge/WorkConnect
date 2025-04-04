2e5d2d7c52f424d6844888e4911ebb848891c20d
Aretha Pierre <aretha.pierre@iths.se>
2025-04-04 22:22:44 +0200

    1. Coderabbit fixes:
    * AuthController: consider removing @GetMapping("/login") spring security handles login.
    
    * Create style.css and move <style> from logout.html, home.html and dashboard.html to style.css
    
    * Add public folder to static folder. Add image and style.css to this folder. this to be able to see style.css and image without needing to login. add public to the style.css tag in html files and to the image in home.html
    
    * Change Default encoding for properties file to UTF-8
    
    * Change year tag in home.html to update automatically

--- src/main/java/se/iths/java24/spring25/controllers/AuthController.java
+++ src/main/java/se/iths/java24/spring25/controllers/AuthController.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AuthController {
 
-    @GetMapping("/login")
-    public String showLoginForm() {
-        return "home"; // This will look for login.html in templates folder
-    }
+    // This isn't being used due to Spring boot security own login
+//    @GetMapping("/login")
+//    public String showLoginForm() {
+//        return "login"; // This will look for login.html in templates folder
+//    }
 
     @GetMapping("/")
     public String showHomePage() {
