113ce6cad166e7ee0f10b6119ef8d685968990ce
Anton <antongrubisic@outlook.com>
2025-04-11 10:14:05 +0200

    Changing userService to hash the password

--- src/main/java/se/iths/java24/spring25/repository/UserRepository.java
+++ src/main/java/se/iths/java24/spring25/repository/UserRepository.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
     Optional<UserEntity> findByEmail(String email);
     List<UserEntity> findByName(String name);
 }
--- src/main/java/se/iths/java24/spring25/service/UserService.java
+++ src/main/java/se/iths/java24/spring25/service/UserService.java
@@ -1,19 +1,23 @@
 package se.iths.java24.spring25.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import se.iths.java24.spring25.entity.UserEntity;
 import se.iths.java24.spring25.repository.UserRepository;
 
 import java.util.List;
 import java.util.Optional;
+// glöm inte att ha med detta ↑ om du inte redan importerat
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public List<UserEntity> getAllUsers() {
@@ -32,10 +36,18 @@ public class UserService {
         userRepository.deleteById(id);
     }
 
-    public UserEntity updateUser(UserEntity user ) {
+    public UserEntity updateUser(UserEntity user) {
         return userRepository.save(user);
     }
 
-    public void registerUser(String email, String password) {
+
+    public void registerUser(String name, String email, String password) {
+        if (userRepository.findByEmail(email).isPresent()) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        String hashedPassword = passwordEncoder.encode(password);
+        UserEntity user = new UserEntity(name, email, hashedPassword);
+        userRepository.save(user);
     }
 }
