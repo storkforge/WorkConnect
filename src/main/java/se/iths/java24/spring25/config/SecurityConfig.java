a7f9a9def74a24fad4639400ce382de0cfc6484e
Anton <antongrubisic@outlook.com>
2025-04-13 11:28:19 +0200

    Added passWordEncoder to Securityconfig instead, tried it and it worked with the database so the password now is encrypted

--- src/main/java/se/iths/java24/spring25/config/SecurityConfig.java
+++ src/main/java/se/iths/java24/spring25/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package se.iths.java24.spring25.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,18 +14,16 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public/**", "/register", "/login", "/error", "/", "/css/**", "/js/**", "/images/**", "/download.png").permitAll() // <-- Lade till vanliga statiska resurser och din logo
+                        .requestMatchers("/public/**", "/register", "/login", "/error", "/", "/css/**", "/js/**", "/images/**", "/download.png").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
-                                .loginPage("/login")
-                                .defaultSuccessUrl("/dashboard", true)
-                                //.failureUrl("/login?error=true") // If needed: error.html, needs to be implemented
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/dashboard", true)
                 )
                 .oauth2Login(oauth2 -> oauth2
-                                .loginPage("/login")
-                                .defaultSuccessUrl("/dashboard", true)
-                        // .failureUrl("/login?error=true") //
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/dashboard", true)
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
@@ -32,5 +32,10 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
