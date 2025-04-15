78f6560317c7b959ed7c14a70c3a3d04b1d50732
Anton <antongrubisic@outlook.com>
2025-04-15 14:24:03 +0200

    Altered some migrations

--- src/main/resources/db/migration/V4__Insert_Admin_Into_Users.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (name, email, password, provider, provider_id)
-VALUES
-    ('admin', 'admin@workconnect.com', '$2a$10$7eWf6jKZo...dinBcryptHash...', 'LOCAL', NULL);
---
+++ src/main/resources/db/migration/V7__Insert_admin_user.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, email, password, provider, provider_id)
+VALUES (
+           'admin',
+           'admin@workconnect.com',
+           '$2a$10$ZgN93h4uIsbk1UF1fwZt/OwNRMi1LbYpbUoJ.zZuPtGT.5qpHhWhC',
+           'LOCAL',
+           NULL
+       );
