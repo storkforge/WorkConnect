552bdeae9ad9135fa8f81e795dd9d71c4c42c5ef
Stelle Simonlatser <stelle83@gmail.com>
2025-04-17 10:45:34 +0200

    connect job registration with DB. Please note, V3_JobOpportunity.sql removed

--- src/main/java/se/iths/java24/spring25/controllers/web/JobWebController.java
+++ src/main/java/se/iths/java24/spring25/controllers/web/JobWebController.java
@@ -1,8 +1,15 @@
 package se.iths.java24.spring25.controllers.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import se.iths.java24.spring25.repository.JobOpportunityRepository;
 import se.iths.java24.spring25.service.JobOpportunityService;
 import se.iths.java24.spring25.entity.JobOpportunityEntity;
 
@@ -13,6 +20,7 @@ public class JobWebController {
 
     private final JobOpportunityService jobService;
 
+    @Autowired
     public JobWebController(JobOpportunityService jobService) {
         this.jobService = jobService;
     }
@@ -24,11 +32,45 @@ public class JobWebController {
         return "jobs"; // jobs.html in templates folder
     }
 
+    @Autowired
+    private JobOpportunityRepository jobOpportunityRepository;
+
+
+    public JobWebController(JobOpportunityService jobService, JobOpportunityRepository jobOpportunityRepository) {
+        this.jobService = jobService;
+        this.jobOpportunityRepository = jobOpportunityRepository;
+    }
+
+
+//    @GetMapping("/jobs/jobregistration")
+//    public String RegisterNewJob(Model model) {
+//        List<JobOpportunityEntity> jobs = jobService.getAllJobOpportunities(); // Replace with filtered list if needed
+//        model.addAttribute("jobs", jobs);
+//        return "jobRegister"; // jobs.html in templates folder
+//    }
+
+//    @PostMapping("/jobs/jobregistration")
+//    public String createJob(@ModelAttribute JobOpportunityEntity jobOpportunity) {
+//        jobService.createJobOpportunity(jobOpportunity);
+//        return "redirect:/jobs";
+//    }
+
     @GetMapping("/jobs/jobregistration")
-    public String RegisterNewJob(Model model) {
-        List<JobOpportunityEntity> jobs = jobService.getAllJobOpportunities(); // Replace with filtered list if needed
-        model.addAttribute("jobs", jobs);
-        return "jobRegister"; // jobs.html in templates folder
+    public String showJobRegistrationForm(Model model) {
+        model.addAttribute("jobOpportunity", new JobOpportunityEntity());
+        return "jobRegister"; // jobs/jobregistration.html in templates folder
+    }
+
+    @PostMapping("/jobs/jobregistration")
+    public String submitForm(@ModelAttribute JobOpportunityEntity jobOpportunity) {
+        jobOpportunityRepository.save(jobOpportunity);
+        return "redirect:/jobs/jobSuccess"; // see vist VALE!!!
+    }
+
+    // Optional: show success page
+    @GetMapping("/jobs/jobSuccess")
+    public String showSuccess() {
+        return "jobSuccess";
     }
 
 }
--- src/main/java/se/iths/java24/spring25/entity/JobOpportunityEntity.java
+++ src/main/java/se/iths/java24/spring25/entity/JobOpportunityEntity.java
@@ -5,15 +5,45 @@ import jakarta.persistence.*;
 
 
 @Entity
+@Table(name = "job_opportunity_entity")
 public class JobOpportunityEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String description;
+    private String description; // from V3
+
+    @Column(name = "company_name")
+    private String companyName; // from V9
+
+    @Column(name = "job_title", unique = true)
+    private String jobTitle;// from V9 unique
+
+    @Column(name = "job_description")
+    private String jobDescription; // from V9
+
+    @Column(name = "location")
+    private String location; // from V9
+
+    @Column(name = "terms_of_employment")
+    private String termsOfEmployment; // from V9
+
 
     @ManyToOne
     @JoinColumn(name = "event_id")
     private EventEntity event;
+
+    public JobOpportunityEntity() {}
+
+    public JobOpportunityEntity(String companyName, String jobTitle, String jobDescription, String location, String termsOfEmployment) {
+        this.companyName = companyName;
+        this.jobTitle = jobTitle;
+        this.jobDescription = jobDescription;
+        this.location = location;
+        this.termsOfEmployment = termsOfEmployment;
+    }
+
+
 
     // Getters and setters
     public Long getId() {
@@ -32,11 +62,69 @@ public class JobOpportunityEntity {
         this.description = description;
     }
 
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getJobDescription() {
+        return jobDescription;
+    }
+
+    public void setJobDescription(String jobDescription) {
+        this.jobDescription = jobDescription;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getTermsOfEmployment() {
+        return termsOfEmployment;
+    }
+
+    public void setTermsOfEmployment(String termsOfEmployment) {
+        this.termsOfEmployment = termsOfEmployment;
+    }
+
     public EventEntity getEvent() {
         return event;
     }
 
     public void setEvent(EventEntity event) {
         this.event = event;
+    }
+
+    public void setProvider(AuthProvider authProvider) {
+    }
+
+    public void setProviderId(Object o) {
+    }
+
+    @Override
+    public String toString() {
+        return "JobOpportunityEntity{" +
+                "id=" + id +
+                "companyName='" + companyName + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", jobDescription='" + jobDescription + '\'' +
+                ", location='" + location + '\'' +
+                ", termsOfEmployment='" + termsOfEmployment + '\'' +
+                '}';
     }
 }
