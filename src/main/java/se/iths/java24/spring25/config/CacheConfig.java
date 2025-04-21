89abea196294f034b97fbf1bf2c2205e54b30ef0
Anton <antongrubisic@outlook.com>
2025-04-21 13:55:10 +0200

    Added cache config

---
+++ src/main/java/se/iths/java24/spring25/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package se.iths.java24.spring25.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
